90db4a369cb6521e9602d3f3b9bf33f00b3e2ec7
Brizeh <alexislamusique@gmail.com>
2023-10-02 12:13:42 +0200

    First commit

---
+++ sim.py
@@ -0,0 +1,106 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from specialization import*
+from collections import Counter
+
+def shortest_x(spe_list):      #Obtenir le temps le plus court de 'spe_list' pour reajuster la longueur des autres tableaux 
+    x_min = len(spe_list[0].x)
+    for spes in spe_list:
+        if(len(spes.x)<x_min):
+            x_min = len(spes.x)
+    for spes in spe_list:
+        spes.x, spes.pdps, spes.cdps, spes.pdmg, spes.cdmg = spes.x[:x_min], spes.pdps[:x_min], spes.cdps[:x_min], spes.pdmg[:x_min], spes.cdmg[:x_min]
+    return
+
+def sortSpe(spe_list):  # Fonction copie des données des objets
+    spes = []
+    for e in spe_list:
+        perso = []
+        perso.append(e.seuil)
+        perso.append(e.pdps)
+        perso.append(e.cdps)
+        spes.append(perso)
+    return spes 
+
+def reajust_dmg(boss_HP,boss_armor,spe_list):
+
+    spes = sortSpe(spe_list) # Copie des données des objets Spé
+    Tdps = np.zeros(len(spe_list[0].x)) # Dps instantané total
+    seuils_up = [] # Compteur de seuils type UP (comme Bsw)
+    seuils_down = [] # Compteur de seuils type DOWN
+
+    for e in spes:  # Appliquer les degats de seuils sur tout le fight
+        e[1]=e[1]*2597/boss_armor
+        if(e[0][0]=="up"):
+            e[1]=e[1]*e[0][2]
+            e[2]=e[2]*e[0][2]
+            if(e[0][1] not in seuils_up):
+                seuils_up.append(e[0][1])
+        Tdps=Tdps*1+e[1]+e[2]
+
+    Tdmg = [0]  # Mettre à jour les degats pour évaluer les seuils up
+    for i in range(1,len(Tdps)):
+        Tdmg.append(np.sum(Tdps[0:i]))
+    Tdmg = np.array(Tdmg)
+
+    for i in range(len(seuils_up)):    #Enlever les seuil up par ordre décroissant
+        dmgSeuil = boss_HP*(1-seuils_up[i]/100)
+        idmgSeuil = np.where(Tdmg>=dmgSeuil)[0][0]
+        Tdps = np.zeros(len(spe_list[0].x))
+        for e in spes:
+            if(e[0][1]==seuils_up[i]):
+                e[1][idmgSeuil:]=e[1][idmgSeuil:]/e[0][2]
+                e[2][idmgSeuil:]=e[2][idmgSeuil:]/e[0][2]
+            Tdps=Tdps*1+e[1]+e[2]            
+
+    Tdmg = [0]  # Mettre à jour les degats pour évaluer les seuils down (comme BTTH)
+    for i in range(1,len(Tdps)):
+        Tdmg.append(np.sum(Tdps[0:i]))
+    Tdmg = np.array(Tdmg)
+
+    iKill = np.where(Tdmg>=boss_HP)[0][0]  # Reduire le graph au moment ou ya kill du BOSS
+    Tdmg = Tdmg[:iKill+1]
+    xTdmg = np.arange(len(Tdmg))
+
+    return xTdmg,Tdmg
+
+def get_color_comp(spe_list): # Get la couleur de comp
+    c = np.zeros(3)
+    for e in spe_list:
+        c = c + e.color
+    c = c/len(spe_list)/255
+    return c
+
+def get_name_comp(spe_list): # Get les noms de spé
+    names = []
+    for e in spe_list:
+        names.append(e.name)
+    names = Counter(names)
+    s = ""
+    for e in names:
+        s = s + str(names[e]) + " " + e + " | "
+    s = s[0:len(s)-2]
+    return s
+
+def graph_comp(boss,spe_list): # Grapher un boss avec une compo
+    shortest_x(spe_list)
+    x,y = reajust_dmg(*boss[:-1],spe_list)
+    n = get_name_comp(spe_list)
+    c = get_color_comp(spe_list)
+    plt.plot(y,marker='o',label=n,color=c)
+    return
+
+def get_title_boss(boss): # Get le titre du graph
+    s = boss[2] + " = " + str(boss[0])+"HP"
+    plt.title(s)
+
+Boss = CAIRN
+
+for i in range(5): # Test de plusieurs compo mélange scourge et bsw
+    graph_comp(Boss,[pBsw]*(10-i*2)+[cScrg]*i*2)
+
+get_title_boss(Boss)
+plt.ticklabel_format(style='plain')
+plt.grid()
+plt.legend(loc='best')
+plt.show()
---
+++ specialization.py
@@ -0,0 +1,59 @@
+import numpy as np
+import csv
+
+################# FONCTIONS D'INITIALISATION DE SPÉ #################
+
+class spe:  #classe
+    def __init__(self, x, pdps, cdps, pdmg, cdmg, color, name, seuil=['non',0,1]):
+        self.x=x
+        self.pdps=pdps
+        self.cdps=cdps
+        self.pdmg=pdmg
+        self.cdmg=cdmg
+        self.color=color
+        self.seuil=seuil
+        self.name=name
+
+def get_spe(name):	#lecture des .csv
+    with open('specializations/'+name+'.csv', 'r') as f:
+        reader = csv.reader(f)
+        data = list(reader)
+        data = np.array(data[1:]).astype(np.float64)
+        pdps = data[0:,2]
+        cdps = data[0:,3]
+        pdmg = data[0:,4]
+        cdmg = data[0:,5]
+        x = np.arange(len(pdps))
+    return x,pdps,cdps,pdmg,cdmg
+
+################# HP/ARMOR DES BOSS #################
+
+VG = [22021440,1910,"VG"]
+GORSEVAL = [21628200,2597,"GORSEVAL"]
+SABETHA = [34015256,2597,"SABETHA"]
+
+SLOTHASOR = [18973828,2597,"SLOTHASOR"]
+MATTHIAS = [25953840,2597,"MATTHIAS"]
+
+KC = [55053600,1910,"KC"]
+XERA_P1 = [11411538,2597,"XERA_P1"]
+XERA_P2 = [12710140,2597,"XERA_P2"]
+
+CAIRN = [19999998,2597,"CAIRN"]
+MO = [22021440,2597,"MO"]
+MOCM = [30000000,2597,"MO CM"]
+SAMAROG = [29493000,2597,"SAMAROG"]
+SAMAROGCM = [40000000,2597,"SAMAROG CM"]
+DEIMOS_P1 = [29216168,2597,"DEIMOS 100-10%"]
+DEIMOS_P2 = [4793274,2597,"DEIMOS 10-0%"]
+DEIMOSCM_P1 = [35525963,2597,"DEIMOS CM 100-10%"]
+DEIMOSCM_P2 = [4844604,2597,"DEIMOS CM 10-0%"]
+
+SH = [35391600,2597,"SH"]
+DHUUM = [32000000,2597,"SH CM"]
+DHUUMCM = [40000000,2597,"DHUUM CM"]
+
+################# LES SPÉ #################
+
+pBsw = spe(*get_spe('pBsw'),np.array([225,217,40]),"pBsw",seuil=["up",80,1.25])
+cScrg = spe(*get_spe('cScrg'),np.array([14,120,15]),"cScrg")
